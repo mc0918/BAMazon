f77edf8dc0d5316e4b98400938d70b03b7ef4702
mc0918 <mcampbell0918@gmail.com>
2019-04-01 09:50:51 -0500

    added node_modules to gitignore

--- bamazon.js
+++ bamazon.js
@@ -46,21 +46,21 @@ function login() {
     });
     console.log(welcome);
     //console.log(res);
-    var table = new Table({ head: ["Item ID", "Product", "Price"] });
+    var table = new Table({ head: ["Item ID", "Product", "Price", "Units"] });
 
     //unfortunately you can't just loop because a new table row won't be created with each entry
     //however, when people add new things, because they're adding one at a time, this is fine
     table.push(
-      [res[0].item_id, res[0].product_name, `$${res[0].price}`],
-      [res[1].item_id, res[1].product_name, `$${res[1].price}`],
-      [res[2].item_id, res[2].product_name, `$${res[2].price}`],
-      [res[3].item_id, res[3].product_name, `$${res[3].price}`],
-      [res[4].item_id, res[4].product_name, `$${res[4].price}`],
-      [res[5].item_id, res[5].product_name, `$${res[5].price}`],
-      [res[6].item_id, res[6].product_name, `$${res[6].price}`],
-      [res[7].item_id, res[7].product_name, `$${res[7].price}`],
-      [res[8].item_id, res[8].product_name, `$${res[8].price}`],
-      [res[9].item_id, res[9].product_name, `$${res[9].price}`]
+      [res[0].item_id, res[0].product_name, `$${res[0].price}`, res[0].units],
+      [res[1].item_id, res[1].product_name, `$${res[1].price}`, res[1].units],
+      [res[2].item_id, res[2].product_name, `$${res[2].price}`, res[2].units],
+      [res[3].item_id, res[3].product_name, `$${res[3].price}`, res[3].units],
+      [res[4].item_id, res[4].product_name, `$${res[4].price}`, res[4].units],
+      [res[5].item_id, res[5].product_name, `$${res[5].price}`, res[5].units],
+      [res[6].item_id, res[6].product_name, `$${res[6].price}`, res[6].units],
+      [res[7].item_id, res[7].product_name, `$${res[7].price}`, res[7].units],
+      [res[8].item_id, res[8].product_name, `$${res[8].price}`, res[8].units],
+      [res[9].item_id, res[9].product_name, `$${res[9].price}`, res[9].units]
     );
 
     console.log(table.toString());
